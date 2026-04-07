871770fbd7b0121a7fa256e3ffbc8b9b272931fe
Sujal Shrestha <sujal@optmyzr.com>
2026-04-08 01:09:28 +0530

    Migrated to next

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/testTyping/' : '/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
